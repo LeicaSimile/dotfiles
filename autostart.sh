7d70f9a19dcd11ad4a4103a3a56b2506c31b7bfd
LeicaSimile <leicasimile@gmail.com>
2026-03-19 10:15:31 -0400

    Add wait time after starting mpd before running mpdas, remove nm-applet from startup

--- autostart.sh
+++ autostart.sh
@@ -8,16 +8,16 @@ function run {
 # Bar
 run waybar
 
-# Music
-run mpd
-run mpdas
-
 # Automount drives
 run udiskie
 
 # Network manager
-run nm-applet
+#run nm-applet
 
 # Night mode
 gammastep -P -O 3000K
 
+# Music
+run mpd
+sleep 5
+run mpdas
